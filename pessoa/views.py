62fc22bbd4a04909211c03c312bf387aefe36ff4
“Aislan” <aislan.santos@gmail.com>
2022-10-17 22:14:04 -0300

    criada a view e a url de contato das pessoas cadastradas assim como o arquivo de contato_list para listarmos os  contatos das pessoas, iniciado as views de crud dos contatos.

--- pessoa/views.py
+++ pessoa/views.py
@@ -1,6 +1,7 @@
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from .models import Pessoa
+from .models import Pessoa, Contato
 from .forms import PessoaForm
 
 
@@ -32,3 +33,21 @@ class PessoaUpdateView(UpdateView):
 class PessoaDeleteView(DeleteView):
     model = Pessoa
     success_url = '/pessoas/'
+
+
+def contatos(request, pk_pessoa):
+    contatos = Contato.objects.filter(pessoa=pk_pessoa)
+    return render(request, 'contato/contato_list.html', {'contatos': contatos, 'pk_pessoa': pk_pessoa})
+
+
+def contato_novo(request, pk_pessoa):
+    form = ContatoForm()
+    if request.method == "POST":
+        form = ContatoForm(request.POST)
+        if form.is_valid():
+            contato = form.save(commit=False)
+            contato.pessoa_id = pk_pessoa
+            contato.save()
+            return redirect(reverse('pessoa.contatos', args=[pk_pessoa]))
+
+    return render(request, 'contato/contato_form.html', {'form': form})
